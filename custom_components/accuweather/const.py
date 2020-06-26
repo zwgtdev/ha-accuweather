a29bcea2a68fc18985d7d5fc46ac1293807a18d6
Maciej Bieniek <maciej.bieniek@gmail.com>
2020-06-26 10:04:45 +0200

    Improve condition classes

--- custom_components/accuweather/const.py
+++ custom_components/accuweather/const.py
@@ -8,20 +8,20 @@ DOMAIN = "accuweather"
 UNDO_UPDATE_LISTENER = "undo_update_listener"
 
 CONDITION_CLASSES = {
+    "clear-night": [33, 34, 37],
     "cloudy": [7, 8, 38],
+    "exceptional": [24, 30, 31],
     "fog": [11],
     "hail": [25],
-    "lightning": [],
-    "lightning-rainy": [15, 16, 17, 41, 42],
+    "lightning": [15],
+    "lightning-rainy": [16, 17, 41, 42],
     "partlycloudy": [4, 6, 35, 36],
     "pouring": [18],
     "rainy": [12, 13, 14, 26, 39, 40],
     "snowy": [19, 20, 21, 22, 23, 43, 44],
     "snowy-rainy": [29],
-    "sunny": [1, 2, 3, 5, 33, 34, 37],
+    "sunny": [1, 2, 3, 5],
     "windy": [32],
-    "windy-variant": [],
-    "exceptional": [24, 30, 31],
 }
 OPTIONAL_SENSORS = (
     "DewPoint",
